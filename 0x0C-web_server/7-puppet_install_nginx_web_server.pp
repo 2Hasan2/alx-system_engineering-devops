a815ed6d48a806c88521ac8f779ad232b65ab525
2Hasan2 <h01554176846@gmail.com>
2024-05-15 18:18:30 +0300

    all done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,27 +1,6 @@
-#!/usr/bin/pup
-# Nginx web server (w/ Puppet)
-package { 'nginx':
-  ensure => installed,
-}
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-file_line { 'default':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-file { '/var/www/html/error_404.html':
-  content => 'Ceci n\'est pas une page',
-}
-file_line { 'error404':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  line   => 'error_page 404 /error_404.html;',
-}
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+# Installs a Nginx server
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
